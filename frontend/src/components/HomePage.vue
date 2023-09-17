c10689dbb47b9e8e302fbd94684c6dd2795e491b
UncleLukie <lukehewitt@smashit.games>
2023-09-17 14:33:43 +1000

    Homepage Mobile Responsiveness

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -78,80 +78,110 @@ export default {
 </script>
 
 <style scoped>
-  .full-width-image {
-    width: 100%;
-  }
+.full-width-image {
+  width: 100%;
+}
 
-  .hero-section {
-    position: relative;
-  }
+.hero-section {
+  position: relative;
+}
 
-  .hero-content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    color: white;
-  }
+.hero-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+}
 
-  .alternating-section .content-wrapper {
-    display: flex;
-    gap: 2rem;
-    align-items: center;
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 2rem 0;
-  }
+.alternating-section .content-wrapper {
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem 0;
+}
 
-  .image-section img {
-    max-width: 300px;
-  }
+.image-section img {
+  max-width: 300px;
+}
 
+.hero-section h1 {
+  font-family: 'Cinzel', serif;
+  font-size: 5rem;
+  font-weight: 700;
+  color: #ffffff;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  margin: 0;
+  line-height: 1.2;
+}
+
+.hero-section p,
+.text-section p {
+  font-family: 'Bitter', serif;
+  font-size: 1.5rem;
+  color: #ffffff;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+}
+
+.text-section h2 {
+  font-family: 'Metamorphous', cursive;
+  font-size: 2.5rem;
+  font-weight: 500;
+  color: #ffffff;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+}
+
+footer {
+  font-family: 'Cormorant Garamond', serif;
+  padding: 2rem 0;
+  text-align: center;
+  color: white;
+}
+
+/* Default styles assuming dark theme */
+.text-section h2,
+.text-section p,
+footer {
+  color: #ffffff;
+}
+
+/* Override styles for light theme */
+.v-theme--light .text-section h2,
+.v-theme--light .text-section p,
+.v-theme--light footer {
+  color: black;
+}
+
+@media (max-width: 768px) {
   .hero-section h1 {
-    font-family: 'Cinzel', serif;
-    font-size: 5rem;
-    font-weight: 700;
-    color: #ffffff;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
-    margin: 0;
-    line-height: 1.2;
+    font-size: 2.5rem; /* Reduced font size for mobile */
   }
 
   .hero-section p,
   .text-section p {
-    font-family: 'Bitter', serif;
-    font-size: 1.5rem;
-    color: #ffffff;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+    font-size: 1rem; /* Reduced font size for mobile */
   }
 
   .text-section h2 {
-    font-family: 'Metamorphous', cursive;
-    font-size: 2.5rem;
-    font-weight: 500;
-    color: #ffffff;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+    font-size: 1.5rem; /* Reduced font size for mobile */
+  }
+
+  .alternating-section .content-wrapper {
+    flex-direction: column; /* Stack image and text on top of each other for mobile */
+    gap: 1rem;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+
+  .image-section img {
+    max-width: 100%; /* Ensure image doesn't exceed screen width */
   }
 
   footer {
-    font-family: 'Cormorant Garamond', serif;
-    padding: 2rem 0;
-    text-align: center;
-    color: white;
+    font-size: 0.9rem; /* Reduced font size for footer on mobile */
   }
-
-  /* Default styles assuming dark theme */
-  .text-section h2,
-  .text-section p,
-  footer {
-    color: #ffffff;
-  }
-
-  /* Override styles for light theme */
-  .v-theme--light .text-section h2,
-  .v-theme--light .text-section p,
-  .v-theme--light footer {
-    color: black;
-  }
+}
 </style>
